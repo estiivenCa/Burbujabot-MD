8680e8de4a0c4bdc7e46d671cf30f4f863f87ba8
Stiiven <122229140+estiivenCa@users.noreply.github.com>
2024-12-02 01:25:16 -0500

    Update

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-    let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Stiiven⁩;;\nFN:Stiiven⁩\nTITLE:\nitem1.TEL;waid=573204545069:573204545069\nitem1.X-ABLabel:Stiiven\nitem2.URL:https://wa.me/573204545069\nitem2.X-ABLabel:Enviar Mensaje\nEND:VCARD`;
-    await conn.sendMessage(m.chat, { contacts: { displayName: 'KanBot⁩', contacts: [{ vcard }] }}, { quoted: m });
+    let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Stiiven⁩;;\nFN:Stiiven⁩\nTITLE:\nitem1.TEL;waid=524151586570:524151586570\nitem1.X-ABLabel:Burbuja\nitem2.URL:https://wa.me/524151586570\nitem2.X-ABLabel:Enviar Mensaje\nEND:VCARD`;
+    await conn.sendMessage(m.chat, { contacts: { displayName: 'BurbujaBot⁩', contacts: [{ vcard }] }}, { quoted: m });
 }
 
 handler.help = ['owner'];
